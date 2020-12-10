36db91c3b8cade5a024499a09360a5ea08488e33
Jakub Štellner <57998476+Stelny@users.noreply.github.com>
2020-12-10 15:51:41 +0100

    Fetch/Preloader

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-
-  function getHeight () {
-
-                         return document.body.scrollHeight;
-  }
   $(".masonry-item img").mousemove(function(e){
     $(this).siblings(".box").css("display", "flex");
     $(".box").css('top',e.pageY+15);
@@ -11,6 +6,26 @@ $(document).ready(function(){
   });
   $(".masonry-item img").mouseleave(function(e){
     $(this).siblings(".box").css("display", "none");
-    console.log("h");
+  });
+
+  $(window).scroll(function(){
+    var sticky = $('header');
+    var margin = $('.start-content');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1) {
+      sticky.addClass('fixed');
+      margin.addClass("margin");
+    }
+    else {
+      sticky.removeClass('fixed');
+      margin.removeClass("margin");
+    }
+  });
+
+  $(window).load(function() {
+  	setTimeout(function(){
+  			$("#preloader").css("opacity", "0");
+  	}, 2000)
   });
 });
